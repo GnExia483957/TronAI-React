c538bf5dfb5db8c897a44a1a539b0ceeeeb22c5d
NgaiGarrick <garrick.ngai@tron.network>
2024-10-04 16:03:32 +0800

    centered left the top logo

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
+import logoImage from '../../assets/images/Tron-Icon.png'; // Adjust the import path as necessary
 
 
 const Navbar = () => {
     return (
         <header>
-            <div id="logo"><Link to="/">Tron.AI</Link></div>
+            <div id="logo">
+                <img id="logo-image" src={logoImage} alt="logo" />
+                <a id="nav-title" href="index.html">
+                    <div id="title">TRON.AI</div>
+                </a>
+            </div>
+
             <nav>
                 <Link to="/">News</Link>
                 <Link to="/AI">AI</Link>
