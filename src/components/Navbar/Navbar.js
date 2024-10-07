d784bca884bd78e38c278c5ba1d0fe84f2e3585f
NgaiGarrick <garrick.ngai@tron.network>
2024-10-07 15:15:00 +0800

    integrated modal back to homepage

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,16 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
 import agentImage from '../../assets/images/Agent-Icon.png';
 
+import '../Modal/Modal.css';
+import Agent from '../Agent/Chatbot'
+
+
+const Modal = ({ isOpen, onClose }) => {
+    return (
+      <div className={`modal ${isOpen ? 'open' : ''}`}>
+        <div className="modal-content">
+          <span className="close" onClick={onClose}>&times;</span>
+          <Agent/>
+        </div>
+      </div>
+    );
+  };
+
+
 const Navbar = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const openModal = () => setIsOpen(true);
+    const closeModal = () => setIsOpen(false);
+  
     return (
             <nav>
                 <Link to="/">News</Link>
                 <Link to="/AI">AI</Link>
-                <Link to="/Agent"><img id="agent-image" src={agentImage} alt="logo" /></Link>
-                <Link to="/Test">Test</Link>
+                {/* <Link to="/Test">Test</Link> */}
+                <div onClick={openModal}><img id="agent-image" src={agentImage} alt="logo" /></div>
+                  <Modal isOpen={isOpen} onClose={closeModal} />
             </nav>
+
+
+
+
     );
 };
 
