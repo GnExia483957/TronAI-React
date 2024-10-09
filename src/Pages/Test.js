a11ddfdcdbe60cfbae8e3146d7aefde68ab3f640
NgaiGarrick <garrick.ngai@tron.network>
2024-10-09 18:12:10 +0800

    removed unnecessary component

--- src/Pages/Test.js
+++ src/Pages/Test.js
@@ -1,12 +1,9 @@
 import React from 'react';
-import ChatModal from '../components/Modal/Modal';
-import Navbar from '../components/Navbar/Navbar';
+
 const Test = () => {
 
   return (
     <div>
-      <Navbar/>
-      <ChatModal/>
     </div>
   );
 };
